b2ba2664816e139bcc8293835f4471448742d1b5
Jared Hocutt <jhocutt@redhat.com>
2020-04-10 10:45:34 -0400

    WIP: Create base infrastructure for OpenShift 4
    Signed-off-by: Jared Hocutt <jhocutt@redhat.com>

--- playbooks/roles/infrastructure/terraform/main.tf
+++ playbooks/roles/infrastructure/terraform/main.tf
@@ -314,7 +314,7 @@ resource "aws_route_table_association" "private2" {
 ###############################################################################
 
 resource "aws_lb" "masters_ext" {
-  name               = "${var.cluster_name}-masters-ext"
+  name               = "${var.cluster_name}-ext"
   internal           = true
   load_balancer_type = "network"
 
@@ -327,13 +327,13 @@ resource "aws_lb" "masters_ext" {
   tags = merge(
     local.common_tags,
     map(
-      "Name", "${var.cluster_name}-masters-ext"
+      "Name", "${var.cluster_name}-ext"
     )
   )
 }
 
 resource "aws_lb" "masters_int" {
-  name               = "${var.cluster_name}-masters-int"
+  name               = "${var.cluster_name}-int"
   internal           = true
   load_balancer_type = "network"
 
@@ -346,7 +346,7 @@ resource "aws_lb" "masters_int" {
   tags = merge(
     local.common_tags,
     map(
-      "Name", "${var.cluster_name}-masters-int"
+      "Name", "${var.cluster_name}-int"
     )
   )
 }
@@ -408,36 +408,36 @@ resource "aws_lb_listener" "machine_config" {
 resource "aws_lb_target_group_attachment" "api_masters" {
   count = 3
 
-  target_group_arn = "${aws_lb_target_group.api.arn}"
-  target_id        = "${aws_instance.masters[count.index].id}"
+  target_group_arn = aws_lb_target_group.api.arn
+  target_id        = aws_instance.masters[count.index].id
   port             = 6443
 }
 
 resource "aws_lb_target_group_attachment" "api_int_masters" {
   count = 3
 
-  target_group_arn = "${aws_lb_target_group.api_int.arn}"
-  target_id        = "${aws_instance.masters[count.index].id}"
+  target_group_arn = aws_lb_target_group.api_int.arn
+  target_id        = aws_instance.masters[count.index].id
   port             = 6443
 }
 
 resource "aws_lb_target_group_attachment" "api_int_bootstrap" {
-  target_group_arn = "${aws_lb_target_group.api_int.arn}"
-  target_id        = "${aws_instance.bootstrap.id}"
+  target_group_arn = aws_lb_target_group.api_int.arn
+  target_id        = aws_instance.bootstrap.id
   port             = 6443
 }
 
 resource "aws_lb_target_group_attachment" "machine_config_masters" {
   count = 3
 
-  target_group_arn = "${aws_lb_target_group.machine_config.arn}"
-  target_id        = "${aws_instance.masters[count.index].id}"
+  target_group_arn = aws_lb_target_group.machine_config.arn
+  target_id        = aws_instance.masters[count.index].id
   port             = 22623
 }
 
 resource "aws_lb_target_group_attachment" "machine_config_bootstrap" {
-  target_group_arn = "${aws_lb_target_group.machine_config.arn}"
-  target_id        = "${aws_instance.bootstrap.id}"
+  target_group_arn = aws_lb_target_group.machine_config.arn
+  target_id        = aws_instance.bootstrap.id
   port             = 22623
 }
 
@@ -475,18 +475,18 @@ resource "aws_security_group_rule" "bastion_ingress_ssh" {
   cidr_blocks = ["0.0.0.0/0"]
 }
 
-resource "aws_security_group" "cluster" {
-  name        = "${var.cluster_name}-cluster"
-  description = "${var.cluster_name} security group"
+resource "aws_security_group" "bootstrap" {
+  name        = "${var.cluster_name}-bootstrap"
+  description = "${var.cluster_name} bootstrap security group"
   vpc_id      = aws_vpc.openshift.id
 
   tags = {
-    Name = "${var.cluster_name}-cluster"
+    Name = "${var.cluster_name}-bootstrap"
   }
 }
 
-resource "aws_security_group_rule" "cluster_egress" {
-  security_group_id = aws_security_group.cluster.id
+resource "aws_security_group_rule" "bootstrap_egress" {
+  security_group_id = aws_security_group.bootstrap.id
   type              = "egress"
 
   from_port   = 0
@@ -495,8 +495,8 @@ resource "aws_security_group_rule" "cluster_egress" {
   cidr_blocks = ["0.0.0.0/0"]
 }
 
-resource "aws_security_group_rule" "cluster_ingress_vpc" {
-  security_group_id = aws_security_group.cluster.id
+resource "aws_security_group_rule" "bootstrap_ingress_vpc" {
+  security_group_id = aws_security_group.bootstrap.id
   type              = "ingress"
 
   from_port   = 0
@@ -505,12 +505,102 @@ resource "aws_security_group_rule" "cluster_ingress_vpc" {
   cidr_blocks = [var.vpc_cidr]
 }
 
+resource "aws_security_group" "master" {
+  name        = "${var.cluster_name}-master"
+  description = "${var.cluster_name} master security group"
+  vpc_id      = aws_vpc.openshift.id
+
+  tags = {
+    Name = "${var.cluster_name}-master"
+  }
+}
+
+resource "aws_security_group_rule" "master_egress" {
+  security_group_id = aws_security_group.master.id
+  type              = "egress"
+
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "master_ingress_vpc" {
+  security_group_id = aws_security_group.master.id
+  type              = "ingress"
+
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = [var.vpc_cidr]
+}
+
+resource "aws_security_group" "worker" {
+  name        = "${var.cluster_name}-worker"
+  description = "${var.cluster_name} worker security group"
+  vpc_id      = aws_vpc.openshift.id
+
+  tags = {
+    Name = "${var.cluster_name}-worker"
+  }
+}
+
+resource "aws_security_group_rule" "worker_egress" {
+  security_group_id = aws_security_group.worker.id
+  type              = "egress"
+
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "worker_ingress_vpc" {
+  security_group_id = aws_security_group.worker.id
+  type              = "ingress"
+
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = [var.vpc_cidr]
+}
+
+# resource "aws_security_group" "cluster" {
+#   name        = "${var.cluster_name}-cluster"
+#   description = "${var.cluster_name} security group"
+#   vpc_id      = aws_vpc.openshift.id
+
+#   tags = {
+#     Name = "${var.cluster_name}-cluster"
+#   }
+# }
+
+# resource "aws_security_group_rule" "cluster_egress" {
+#   security_group_id = aws_security_group.cluster.id
+#   type              = "egress"
+
+#   from_port   = 0
+#   to_port     = 0
+#   protocol    = "-1"
+#   cidr_blocks = ["0.0.0.0/0"]
+# }
+
+# resource "aws_security_group_rule" "cluster_ingress_vpc" {
+#   security_group_id = aws_security_group.cluster.id
+#   type              = "ingress"
+
+#   from_port   = 0
+#   to_port     = 0
+#   protocol    = "-1"
+#   cidr_blocks = [var.vpc_cidr]
+# }
+
 ###############################################################################
 # IAM
 ###############################################################################
 
 resource "aws_iam_role" "bootstrap" {
-  name = "${var.cluster_name}-bootstrap"
+  name = "${var.cluster_name}-bootstrap-role"
 
   assume_role_policy = <<-EOF
   {
@@ -568,7 +658,7 @@ resource "aws_iam_instance_profile" "bootstrap" {
 }
 
 resource "aws_iam_role" "master" {
-  name = "${var.cluster_name}-master"
+  name = "${var.cluster_name}-master-role"
 
   assume_role_policy = <<-EOF
   {
@@ -625,6 +715,49 @@ resource "aws_iam_instance_profile" "master" {
   role = aws_iam_role.master.name
 }
 
+resource "aws_iam_role" "worker" {
+  name = "${var.cluster_name}-worker-role"
+
+  assume_role_policy = <<-EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": "sts:AssumeRole",
+        "Principal": {
+          "Service": "ec2.amazonaws.com"
+        },
+        "Effect": "Allow",
+        "Sid": ""
+      }
+    ]
+  }
+  EOF
+}
+
+resource "aws_iam_role_policy" "worker" {
+  name = "${var.cluster_name}-worker-policy"
+  role = aws_iam_role.worker.id
+
+  policy = <<-EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": "ec2:Describe*",
+        "Effect": "Allow",
+        "Resource": "*"
+      }
+    ]
+  }
+  EOF
+}
+
+resource "aws_iam_instance_profile" "worker" {
+  name = "${var.cluster_name}-worker-profile"
+  role = aws_iam_role.worker.name
+}
+
 ###############################################################################
 # EC2
 ###############################################################################
@@ -641,7 +774,7 @@ resource "aws_instance" "bastion" {
     delete_on_termination = true
   }
 
-  vpc_security_group_ids      = [aws_security_group.bastion.id, aws_security_group.cluster.id]
+  vpc_security_group_ids      = [aws_security_group.bastion.id, aws_security_group.master.id]
   iam_instance_profile        = aws_iam_instance_profile.master.name
   associate_public_ip_address = true
 
@@ -661,11 +794,13 @@ resource "aws_instance" "bootstrap" {
     delete_on_termination = true
   }
 
-  vpc_security_group_ids      = [aws_security_group.cluster.id]
-  iam_instance_profile        = aws_iam_instance_profile.master.name
+  vpc_security_group_ids      = [aws_security_group.bootstrap.id, aws_security_group.master.id]
+  iam_instance_profile        = aws_iam_instance_profile.bootstrap.name
   associate_public_ip_address = false
 
-  user_data = "{\"ignition\":{\"config\":{\"replace\":{\"source\":\"http://${aws_instance.bastion.private_ip}/bootstrap.ign\",\"verification\":{}}},\"timeouts\":{},\"version\":\"2.1.0\"},\"networkd\":{},\"passwd\":{},\"storage\":{},\"systemd\":{}}"
+  user_data = <<-EOF
+  {"ignition":{"config":{"replace":{"source":"http://${aws_instance.bastion.private_ip}/bootstrap.ign","verification":{}}},"timeouts":{},"version":"2.1.0"},"networkd":{},"passwd":{},"storage":{},"systemd":{}}
+  EOF
 
   tags = merge(
     local.common_tags,
@@ -690,16 +825,51 @@ resource "aws_instance" "masters" {
     delete_on_termination = true
   }
 
-  vpc_security_group_ids      = [aws_security_group.cluster.id]
+  vpc_security_group_ids      = [aws_security_group.master.id]
   iam_instance_profile        = aws_iam_instance_profile.master.name
   associate_public_ip_address = false
 
-  user_data = "{\"ignition\":{\"config\":{\"replace\":{\"source\":\"http://${aws_instance.bastion.private_ip}/master.ign\",\"verification\":{}}},\"timeouts\":{},\"version\":\"2.1.0\"},\"networkd\":{},\"passwd\":{},\"storage\":{},\"systemd\":{}}"
+  # user_data = "{\"ignition\":{\"config\":{\"replace\":{\"source\":\"http://${aws_instance.bastion.private_ip}/master.ign\",\"verification\":{}}},\"timeouts\":{},\"version\":\"2.1.0\"},\"networkd\":{},\"passwd\":{},\"storage\":{},\"systemd\":{}}"
+  user_data = <<-EOF
+  {"ignition":{"config":{"replace":{"source":"http://${aws_instance.bastion.private_ip}/master.ign","verification":{}}},"timeouts":{},"version":"2.1.0"},"networkd":{},"passwd":{},"storage":{},"systemd":{}}
+  EOF
 
   tags = merge(
     local.common_tags,
     map(
       "Name", "${var.cluster_name}-master-${count.index}"
+    )
+  )
+
+  depends_on = [aws_instance.bastion]
+}
+
+resource "aws_instance" "workers" {
+  count = 3
+
+  instance_type = "m5.xlarge"
+  ami           = var.rhcos_ami
+  subnet_id     = local.private_subnets[count.index].id
+
+  root_block_device {
+    volume_type           = "gp2"
+    volume_size           = 120
+    delete_on_termination = true
+  }
+
+  vpc_security_group_ids      = [aws_security_group.worker.id]
+  iam_instance_profile        = aws_iam_instance_profile.worker.name
+  associate_public_ip_address = false
+
+  # user_data = "{\"ignition\":{\"config\":{\"replace\":{\"source\":\"http://${aws_instance.bastion.private_ip}/worker.ign\",\"verification\":{}}},\"timeouts\":{},\"version\":\"2.1.0\"},\"networkd\":{},\"passwd\":{},\"storage\":{},\"systemd\":{}}"
+  user_data = <<-EOF
+  {"ignition":{"config":{"replace":{"source":"http://${aws_instance.bastion.private_ip}/worker.ign","verification":{}}},"timeouts":{},"version":"2.1.0"},"networkd":{},"passwd":{},"storage":{},"systemd":{}}
+  EOF
+
+  tags = merge(
+    local.common_tags,
+    map(
+      "Name", "${var.cluster_name}-worker-${count.index}"
     )
   )
 
